75ed081e962606b713f7e4678117d5fc46a82d56
DONNEL Peter 247651 <PD247651@canismajor.intra.cea.fr>
2022-02-01 12:11:13 +0100

    Include the theoretical prediction by Albajar

--- EC_source.py
+++ EC_source.py
@@ -5,7 +5,10 @@
 
 # Import useful libraries
 import numpy as np
+import math
 from scipy.special import jn
+from scipy.special import spherical_jn
+
 
 #Physical constants
 charge = 1.602 * 10**(-19)   #C
@@ -13,12 +16,14 @@ mass = 9.109 * 10**(-31)     #kg
 light_speed = 299792458      #m/s
 epsilon0 = 8.854 * 10**(-12) #F.m^{-1}
 
+
 # Plasma inputs
 Ne0 = 1.0 * 10**(19)                  #m^{-3}
-Te0 = 1.0 * 10**3 * 1.602 * 10**(-19) #J Boltzmann constant included
+Te0 = 2.0 * 10**3 * 1.602 * 10**(-19) #J Boltzmann constant included
 B0 = 1.4                              #T
 R0 = 1.0                              #m
 a0 = 0.25                             #m
+
 
 # Beam inputs
 harmonic = 2
@@ -27,15 +32,18 @@ omega_b = 7.8 * 10**10 * 2 * np.pi    #Hz
 W0 = 0.02                             #m
 Power_in = 1                          #W
 
+
 # Numerical imput data
 vmax = 4
 Nv = 200
-Nr = 200
+Nr = 400
+
 
 # Vectors
 Vpar = np.linspace(-vmax,vmax,2*Nv)
 Vperp = np.linspace(0.,vmax,Nv)
 vec_R = np.linspace(R0-a0,R0+a0,Nr)
+
 
 # density and temperature profiles (assume a parabolic profiles)
 vec_Ne = np.zeros(Nr)
@@ -50,6 +58,7 @@ for iR in range(Nr):
 dR = vec_R[2] - vec_R[1]
 dVperp = Vperp[2] -Vperp[1]
 dVpar =  Vpar[2] -Vpar[1]
+
 
 # Compute refractive index
 def compute_N(theta_loc, P_loc, omega_ce_loc, omega_b_loc):
@@ -107,6 +116,7 @@ def compute_Theta2_n(rho, theta, Ntheta, P_loc, omega_ce_loc, omega_b_loc, vpar,
                      (P_loc - (Ntheta*np.sin(theta))**2))**2)
         return (tmp1 + tmp2 + tmp3)**2 / tmp4
 
+
 # Compute the electric field given the power of the beam
 def compute_E2(Power_loc, R_loc, theta_loc, N_theta_loc, omega_p_loc, Omega_ce_loc, omega_b_loc):
     omega2_p = omega_p_loc**2
@@ -122,6 +132,119 @@ def compute_E2(Power_loc, R_loc, theta_loc, N_theta_loc, omega_p_loc, Omega_ce_l
     vg = light_speed * N_theta_loc * f3 / \
          (g1 - Omega_ce_loc*omega2_p*g2/(2*omega_b_loc*np.sqrt(f2)) - N_theta_loc**2 * g3)
     return Power_loc / (np.pi**1.5 * R_loc * W0 * epsilon0 * np.sin(theta_loc)**3 * abs(vg))
+
+
+# Compute gm(zn) and its derivatives
+def Compute_Gm_derivatives(zn, m):
+    if (m==0):
+        Gm = np.sinh(zn) / zn
+        dGm_dx = - np.sinh(zn) / zn**2 + np.cosh(zn) / zn
+        d2Gm_dx2 = (2/zn**3 + 1/zn) * np.sinh(zn) - 2 * np.cosh(zn) / zn**2
+    elif (m==1):
+        Gm = - np.sinh(zn) / zn**2 + np.cosh(zn) / zn
+        dGm_dx = (2/zn**3 + 1/zn) * np.sinh(zn) - 2 * np.cosh(zn) / zn**2
+        d2Gm_dx2 = - (6/zn**4 + 3/zn**2) * np.sinh(zn) + (6/zn**3 + 1/zn) * np.cosh(zn)
+    elif (m==2):
+        Gm = (3/zn**3 + 1/zn) * np.sinh(zn) - 3 * np.cosh(zn) / zn**2
+        dGm_dx = - (9/zn**4 + 4/zn**2) * np.sinh(zn) + (9/zn**3 + 1/zn) * np.cosh(zn)
+        d2Gm_dx2 = (36/zn**5 + 17/zn**3 + 1/zn) * np.sinh(zn) - \
+                   (36/zn**4 + 5/zn**2) * np.cosh(zn)
+    elif (m==3):
+        Gm = - (15/zn**4 + 6/zn**2) *  np.sinh(zn) + (15/zn**3 + 1/zn) * np.cosh(zn)
+        dGm_dx = (60/zn**5 + 27/zn**3 + 1/zn) * np.sinh(zn) - \
+                 (60/zn**4 + 7/zn**2) * np.cosh(zn)
+        d2Gm_dx2 = - (300/zn**6 + 141/zn**4 + 8/zn**2) * np.sinh(zn) + \
+                   (300/zn**5 + 41/zn**3 + 1/zn) * np.cosh(zn)
+    elif (m==4):
+        Gm = (105/zn**5 + 45/zn**3 + 1/zn) *  np.sinh(zn) - (105/zn**4 + 10/zn**2) * np.cosh(zn)
+        dGm_dx = - (525/zn**6 + 240/zn**4 + 11/zn**2) * np.sinh(zn) + \
+                 (525/zn**5 + 65/zn**3 + 1/zn) * np.cosh(zn)
+        d2Gm_dx2 = (3150/zn**7 + 1485/zn**5 + 87/zn**3 + 1/zn) * np.sinh(zn) - \
+                   (3150/zn**6 + 435/zn**4 + 12/zn**2) * np.cosh(zn)
+    else:
+        print('harmonic not handled:', m)
+    return Gm, dGm_dx, d2Gm_dx2
+
+
+# Compute derivatives of the |J_{m+1/2}(zn)|^2 / xn
+def Compute_usefull_derivatives(xn, yn, m):
+    if (4 * xn**2 > yn**2):
+        zn = 0.5 * complex(np.sqrt(4 * xn**2 - yn**2), yn)
+        zn_bar = zn.conjugate()
+        jm_z = spherical_jn(m,zn)
+        jm1_z = spherical_jn(m-1,zn)
+        jm2_z = spherical_jn(m-2,zn)
+        jm_z_bar = spherical_jn(m,zn_bar)
+        jm1_z_bar = spherical_jn(m-1,zn_bar)
+        jm1_jmbar = jm1_z * jm_z_bar
+        z_jm1_jmbar = zn * jm1_z * jm_z_bar
+        z_jm2_jmbar = zn * jm2_z * jm_z_bar
+        z2_jm2_jmbar = zn**2 * jm2_z * jm_z_bar
+        tmp = (1 - m - zn**2 * (m+1) / xn**2) * jm1_z * jm_z_bar
+
+        fx = 2 * np.absolute(jm_z)**2 / np.pi
+        df_dx = - 4 * (m+1) * np.absolute(jm_z)**2 / (np.pi * xn) + \
+                4 * xn * jm1_jmbar.real / (np.pi * zn.real)
+        df_dy = - 2 * z_jm1_jmbar.imag / (np.pi * zn.real)
+        df_dydy = - yn * z_jm1_jmbar.imag / (2 * np.pi * zn.real**3) + \
+                  xn**2 * np.absolute(jm1_z)**2 / (np.pi * zn.real**2) - \
+                  (2 * z_jm1_jmbar.real + z2_jm2_jmbar.real) / (np.pi * zn.real**2)
+        df_dxdy = - (2 * xn / (np.pi * zn.real**2)) * \
+                  (- z_jm1_jmbar.imag / zn.real + tmp.imag + z_jm2_jmbar.imag + \
+                   zn.imag*np.absolute(jm1_z)**2)
+        fx = fx.real
+        df_dx = df_dx.real
+        df_dy = df_dy.real
+        df_dydy = df_dydy.real
+        df_dxdy = df_dxdy.real
+    else:
+        zn_p = 0.5 * (yn  + np.sqrt(yn**2 - 4 * xn**2))
+        zn_m = 0.5 * (yn  - np.sqrt(yn**2 - 4 * xn**2))
+        [Gm_p, dGm_dx_p, d2Gm_dx2_p] = Compute_Gm_derivatives(zn_p, m)
+        [Gm_m, dGm_dx_m, d2Gm_dx2_m] = Compute_Gm_derivatives(zn_m, m)
+        fx = 2 * Gm_p * Gm_m / np.pi
+        df_dx = 4 * xn * (dGm_dx_m*Gm_p - dGm_dx_p*Gm_m) / (np.pi * (zn_p-zn_m))
+        df_dy = 2 * (zn_p*dGm_dx_p*Gm_m - zn_m*dGm_dx_m*Gm_p) / (np.pi * (zn_p-zn_m))
+        df_dydy = 2 * (zn_p**2*d2Gm_dx2_p*Gm_m - zn_m**2*d2Gm_dx2_m*Gm_p - \
+                       2*xn**2*dGm_dx_p*dGm_dx_m) / (np.pi * (zn_p-zn_m)**2)
+        df_dxdy = 4 * xn / (np.pi * (zn_p-zn_m)**2) * \
+                  (dGm_dx_p*Gm_m - dGm_dx_m*Gm_p + yn*dGm_dx_p*dGm_dx_m - \
+                   zn_p*d2Gm_dx2_p*Gm_m - zn_m*d2Gm_dx2_m*Gm_p) 
+        
+    return fx, df_dx, df_dy, df_dydy, df_dxdy
+
+
+# Compute BigA and BigB
+def Compute_A_B(omega_b, omega_p, Omega_ce, N0, theta, n_beam):
+    P_loc = 1 - (omega_p/omega_b)**2
+    Npar = N0 * np.cos(theta)
+    Nperp = N0 * np.sin(theta)
+    n0 = omega_b * np.sqrt(1 - Npar**2) / Omega_ce
+    tmp = omega_b * (omega_p**2 - (omega_b**2-Omega_ce**2) * (1-N0**2)) / \
+              (Omega_ce*omega_p**2)
+    Small_a = (1 + P_loc*(Npar*tmp/(P_loc-Nperp**2))**2) * np.sin(theta)
+    Small_b = abs(1 + P_loc / (P_loc-Nperp**2) * tmp**2) * abs(np.cos(theta))
+    ey = np.sqrt(1 / (N0 * np.sqrt(Small_a**2 + Small_b**2)))
+    normalized_freq = Omega_ce / omega_b
+    R_loc = (P_loc - normalized_freq) / (1 - normalized_freq)
+    L_loc = (P_loc + normalized_freq) / (1 + normalized_freq)
+    S_loc = 0.5 * (R_loc + L_loc)
+    T_loc = 0.5 * (R_loc - L_loc)
+    ex = (S_loc - N0**2) * ey * complex(0,1) / T_loc
+    ez = - Npar * Nperp * ex / (P_loc - Nperp**2)
+    Axz = ex + Npar * Nperp * ez / (1 - Npar**2)
+    xn = omega_b * Nperp * np.sqrt((n_beam/n0)**2 - 1) / Omega_ce
+    yn = mu_loc * Npar * np.sqrt(((n_beam/n0)**2 - 1)/(1 - Npar**2))
+    [fx, df_dx, df_dy, df_dydy, df_dxdy] = Compute_usefull_derivatives(xn, yn, n_beam)
+    [gx, dg_dx, dg_dy, dg_dydy, dg_dxdy] = Compute_usefull_derivatives(xn, yn, n_beam+1)
+    BigA = (np.absolute(Axz)**2 + ey**2) * fx + (Axz*complex(0,ey)).real * xn * df_dx / n_beam - \
+           (xn/n_beam)**2 * (n_beam/(n_beam+1)) * ey**2 * (fx - df_dydy) + \
+           (xn / (n_beam * np.sqrt(1-Npar**2)))**2 * np.absolute(ez)**2 * df_dydy - \
+           xn * (2*(Axz*ez.conjugate()).real * df_dy+ (complex(0,ey)*ez).real * xn * df_dxdy/ n_beam) / \
+           (n_beam * np.sqrt(1-Npar**2))
+    BigB = (xn / n_beam)**2 * (2*n_beam + 3) / ((n_beam+1)*(n_beam+2)) * ey**2 * (gx - dg_dydy)
+    return BigA, BigB
+
 
 # Compute quantities at the entry in the plasma (=outer midplane)
 Ne_in = vec_Ne[Nr-1]
@@ -144,12 +267,15 @@ R_eff_min = - vmax * vT_on_c_max * abs(Npar_in) * R_in + \
             harmonic * Omega_ce0 * R0 * np.sqrt(1 - (vmax*vT_on_c_max)**2) / omega_b
 R_eff_max = + vmax * vT_on_c_max * abs(Npar_in) * R_in + \
             harmonic * Omega_ce0 * R0 * np.sqrt(1 - (vmax*vT_on_c_max)**2) / omega_b
-print(R_res_max,R_eff_max,R_eff_min)
+
 # Allocation of the empty arrays
 vec_Power = np.zeros(Nr)
+vec_Albajar = np.zeros(Nr)
 vec_Power[Nr-1] = Power_in
-
+vec_Albajar[Nr-1] = Power_in
+vec_tau = np.zeros(Nr)
 Dn = np.zeros((Nr,2*Nv,Nv))
+tau_loc = 0.
 for iR in range(Nr-2,-1,-1):
     Power_absorbed = 0.
     R_loc = vec_R[iR] 
@@ -170,6 +296,23 @@ for iR in range(Nr-2,-1,-1):
         E2_loc = compute_E2(Power_loc, R_loc, theta0_loc, N0_loc, omega_p_loc, \
                             Omega_ce_loc, omega_b)
 
+        # Compute the theoretical optical thickness (Albajar)
+        Npar_loc = N0_loc * np.cos(theta0_loc)
+        Nperp_loc = N0_loc * np.sin(theta0_loc)
+        mu_loc = light_speed**2 * mass / Te_loc
+        harmonic0 = omega_b * np.sqrt(1 - Npar_loc**2) / Omega_ce_loc
+        if (math.ceil(harmonic0) == harmonic):
+            [Big_A_loc, Big_B_loc] = Compute_A_B(omega_b, omega_p_loc, Omega_ce_loc, N0_loc, theta0_loc, harmonic)
+            Pn_loc = np.pi*math.factorial(2*harmonic+1) / (2**harmonic*math.factorial(harmonic))**2 * \
+                    (harmonic * Omega_ce_loc / (omega_b * Nperp_loc))**2 * (Big_A_loc + Big_B_loc)
+            Fn_loc = mu_loc**2.5 * Pn_loc * \
+                    np.exp(mu_loc*(1 - harmonic/(harmonic0*np.sqrt(1 - Npar_loc**2))))
+            alpha_loc = omega_p_loc**2 * np.sqrt(0.5*np.pi) * Fn_loc * \
+                        np.sqrt((harmonic/harmonic0)**2 - 1) / \
+                        (light_speed * Omega_ce_loc * harmonic0)
+            tau_loc += alpha_loc * abs(np.sin(theta0_loc)) * dR
+
+            
         # Compute the resonant diffusion coefficient
         for ivpar in range(2*Nv):  
             for ivperp in range(Nv):
@@ -201,7 +344,9 @@ for iR in range(Nr-2,-1,-1):
 
     # Fill the power vector
     vec_Power[iR] = vec_Power[iR+1] - Power_absorbed
-    print("Power absorbed", Power_absorbed)
+    vec_Albajar[iR] = Power_in * np.exp(-tau_loc)
+    if (Power_absorbed > 0.):
+        print("iR", iR)
 
 
 # Save arrays in prevision of their exploitation
@@ -211,6 +356,7 @@ np.save('vec_Te.npy', vec_Te)
 np.save('Vpar.npy', Vpar)
 np.save('Vperp.npy', Vperp)
 np.save('vec_Power.npy', vec_Power)
+np.save('vec_Albajar.npy', vec_Albajar)
 np.save('Dn.npy', Dn)
 
 
